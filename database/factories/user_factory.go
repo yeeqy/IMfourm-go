9e2e400d145a0ffb3fb6b1a4c08293775cb56f4b
yeeqy <www.1737050420@qq.com>
2022-07-06 20:19:53 +0800

    factory 和 faker

---
+++ database/factories/user_factory.go
@@ -0,0 +1,30 @@
+// Package factories 存放工厂方法
+package factories
+
+import (
+	"IMfourm-go/app/models/user"
+	"IMfourm-go/pkg/helpers"
+	"github.com/bxcodec/faker/v3"
+)
+
+//Seeder 功能是往数据库里填充假数据，方便列表接口有数据可用。
+//factory —— 数据工厂，用来批量生成模型对象，且使用假数据对这些模型对象的属性进行赋值；
+//seeder —— 负责将数据工厂里生成的对象插入到数据库中。
+
+func MakeUsers(times int) []user.User{
+	var obj []user.User
+
+	//设置唯一值
+	faker.SetGenerateUniqueValues(true)
+
+	for i:= 0; i < times; i++{
+		model := user.User{
+			Name: faker.Username(),
+			Email: faker.Email(),
+			Phone: helpers.RandomNumber(11),
+			Password:"a/test/password",
+		}
+		obj = append(obj,model)
+	}
+	return obj
+}
