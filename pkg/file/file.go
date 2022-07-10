6362580e4473194887c2f278423fa41f3deab76f
yeeqy <www.1737050420@qq.com>
2022-07-10 22:20:04 +0800

    图片裁剪

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -5,6 +5,7 @@ import (
 	"IMfourm-go/pkg/auth"
 	"IMfourm-go/pkg/helpers"
 	"fmt"
+	"github.com/disintegration/imaging"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"mime/multipart"
@@ -50,7 +51,26 @@ func SaveUploadAvatar(c *gin.Context,file *multipart.FileHeader)(string,error)  
 	if err:= c.SaveUploadedFile(file,avatarPath);err!=nil{
 		return avatar,err
 	}
-	return avatarPath,nil
+
+	//图片裁剪也封装在此
+	img,err := imaging.Open(avatarPath,imaging.AutoOrientation(true))
+	if err != nil {
+		return avatar,err
+	}
+	resizeAvatar := imaging.Thumbnail(img,256,256,imaging.Lanczos)
+	resizeAvatarName := randomNameFromUploadFile(file)
+	resizeAvatarPath := publicPath + dirName + resizeAvatarName
+
+	err = imaging.Save(resizeAvatar,resizeAvatarPath)
+	if err != nil {
+		return avatar,err
+	}
+	//删除老文件
+	err = os.Remove(avatarPath)
+	if err !=nil{
+		return avatar,err
+	}
+	return dirName + resizeAvatarName ,nil
 }
 func randomNameFromUploadFile(file *multipart.FileHeader) string  {
 	return helpers.RandomString(16) + filepath.Ext(file.Filename)
